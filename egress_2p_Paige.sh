bd7e2b89cbe8963790abb94959dd90beeeace5f3
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2023-02-09 21:40:31 +0530

    Made changes to get away with the warning messages while creating Namespaces

--- egress_2p_Paige.sh
+++ egress_2p_Paige.sh
@@ -5,7 +5,8 @@
 # Arguments:
 #   
 ##############################################################################
-echo $private_ip_address
+#echo $private_ip_address
+#echo $private_ip_address
 oc get clusterversion
 MASTER_NODES_COUNT=$(oc get node -l node-role.kubernetes.io/master= --no-headers | wc -l)
 WORKER_NODES_COUNT=$(oc get node -l node-role.kubernetes.io/worker= --no-headers | wc -l)
@@ -13,19 +14,19 @@ echo $MASTER_NODES_COUNT master nodes and  $WORKER_NODES_COUNT worker nodes
 worker_node1=`oc get node -l node-role.kubernetes.io/worker= --no-headers|awk 'NR==1{print $1}'`
 worker_node2=`oc get node -l node-role.kubernetes.io/worker= --no-headers|awk 'NR==2{print $1}'`
 #Reading the ipvalue from global var
-ls $WORKSPACE/flexy-artifacts/workdir/install-dir/ipfile.txt
-private_ip_address=`cat $WORKSPACE/flexy-artifacts/workdir/install-dir/ipfile.txt`
-echo "private_ip_address"
+#ls $WORKSPACE/flexy-artifacts/workdir/install-dir/ipfile.txt
+#private_ip_address=`cat $WORKSPACE/flexy-artifacts/workdir/install-dir/ipfile.txt`
+#echo "private_ip_address"
 
 #Assign the nodes to be eressable
-oc label node  $worker_node1 "k8s.ovn.org/egress-assignable"=""
-oc label node  $worker_node2 "k8s.ovn.org/egress-assignable"=""
+#oc label node  $worker_node1 "k8s.ovn.org/egress-assignable"=""
+#oc label node  $worker_node2 "k8s.ovn.org/egress-assignable"=""
 
 #TO Automatically get the value of ipv4 address and add the number of ip's in the same subnet of ipv4 in the egress object yaml files.
 oc describe node $worker_node1|grep egress -C 3
 #To create 2 egress objects
-oc create -f config_egressip_ovn_ns_qe_podSelector_red.yaml
-oc create -f config_egressip_ovn_ns_qe_podSelector_blue.yaml
+#oc create -f config_egressip_ovn_ns_qe_podSelector_red.yaml
+#oc create -f config_egressip_ovn_ns_qe_podSelector_blue.yaml
 
 #to get egressip's
 oc get egressip>egressip.txt
@@ -45,6 +46,9 @@ do
     oc get pods -n $test_name
     oc get pods;oc label ns test$i department=qe;
 done
+#for i in {1..4};
+#do oc new-project test$i;oc create -f list_for_pods.json;sleep 5;oc get pods;oc label ns test$i department=qe;
+#done
 
 
 #TO Automate on fetching the public ipaddress from the ipecho service that got enabled for the cluster and should be curled from outside
@@ -56,9 +60,9 @@ for i in {3..4};do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{pri
 
 echo "print ing the env variable from pipeline"
 echo $private_ip_address;
-for i in {1..2}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl $private_ip_address:9095);echo $egress;done
+for i in {1..2}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl 10.0.5.115:9095);echo $egress;done
 
-for i in {3..4}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl $private_ip_address:9095);echo $egress;done
+for i in {3..4}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl 10.0.5.115:9095);echo $egress;done
 #cluster is enabled with ipecho service and https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/ocp-common/job/ginkgo-test/ is run successfully 
 
 #Delete all the projects for next iteration
--- egress_4pOld.sh
+++ egress_4pOld.sh
@@ -5,13 +5,18 @@
 # Arguments:
 #   
 ##############################################################################
-
+#echo $private_ip_address
+echo $private_ip_address
 oc get clusterversion
 MASTER_NODES_COUNT=$(oc get node -l node-role.kubernetes.io/master= --no-headers | wc -l)
 WORKER_NODES_COUNT=$(oc get node -l node-role.kubernetes.io/worker= --no-headers | wc -l)
 echo $MASTER_NODES_COUNT master nodes and  $WORKER_NODES_COUNT worker nodes
 worker_node1=`oc get node -l node-role.kubernetes.io/worker= --no-headers|awk 'NR==1{print $1}'`
 worker_node2=`oc get node -l node-role.kubernetes.io/worker= --no-headers|awk 'NR==2{print $1}'`
+#Reading the ipvalue from global var
+ls $WORKSPACE/flexy-artifacts/workdir/install-dir/ipfile.txt
+private_ip_address=`cat $WORKSPACE/flexy-artifacts/workdir/install-dir/ipfile.txt`
+echo "private_ip_address"
 
 #Assign the nodes to be eressable
 oc label node  $worker_node1 "k8s.ovn.org/egress-assignable"=""
@@ -20,8 +25,8 @@ oc label node  $worker_node2 "k8s.ovn.org/egress-assignable"=""
 #TO Automatically get the value of ipv4 address and add the number of ip's in the same subnet of ipv4 in the egress object yaml files.
 oc describe node $worker_node1|grep egress -C 3
 #To create 2 egress objects
-oc create -f /home/sninganu/egress/config_egressip_ovn_ns_qe_podSelector_red.yaml
-oc create -f /home/sninganu/egress/config_egressip_ovn_ns_qe_podSelector_blue.yaml
+oc create -f config_egressip_ovn_ns_qe_podSelector_red.yaml
+oc create -f config_egressip_ovn_ns_qe_podSelector_blue.yaml
 
 #to get egressip's
 oc get egressip>egressip.txt
@@ -29,7 +34,7 @@ oc get egressip>egressip.txt
 #create test projects, and create some test pods in them, label the projects
 
 for i in {1..4};
-do oc new-project test$i;oc create -f /home/sninganu/egress/verification-tests/testdata/networking/list_for_pods.json;sleep 5;oc get pods;oc label ns test$i department=qe;
+do oc new-project test$i;oc create -f list_for_pods.json;sleep 5;oc get pods;oc label ns test$i department=qe;
 done
 
 
@@ -40,12 +45,14 @@ for i in {3..4};do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{pri
 
 #curl to the ipecho service '10.0.13.150' from outside and verify if it hits the egress ip "ipv4":"10.0.48.0/20"
 
-for i in {1..2}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl 10.0.1.77:9095);echo $egress;done
+echo "print ing the env variable from pipeline"
+echo $private_ip_address;
+for i in {1..2}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl $private_ip_address:9095);echo $egress;done
 
-for i in {3..4}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl 10.0.1.77:9095);echo $egress;done
+for i in {3..4}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl $private_ip_address:9095);echo $egress;done
 #cluster is enabled with ipecho service and https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/ocp-common/job/ginkgo-test/ is run successfully 
 
 #Delete all the projects for next iteration
-#for i in {1..4}; do oc delete ns test$i;done
+for i in {1..4}; do oc delete ns test$i;done
 echo "###################################ITERATION COMPLETE###########################################"
 sleep 3
